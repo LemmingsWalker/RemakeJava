3f2f36766cc7e3e3bff2181d3a5da7f63a489450
Doeke Wartena <clankill3r@gmail.com>
2016-05-01 20:18:57 +0200

    A class comment

--- src/com/github/lemmingswalker/ContourDataProcessor.java
+++ src/com/github/lemmingswalker/ContourDataProcessor.java
@@ -4,5 +4,10 @@ package com.github.lemmingswalker;
  * Created by doekewartena on 4/30/16.
  */
 public interface ContourDataProcessor {
+    /**
+     *
+     * @param contourData
+     * @return return true to continue scanning
+     */
     boolean process(ContourData contourData);
 }
